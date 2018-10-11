ebe524ba41603e141a673079ab8172e90da85896
Blake Johnson <brjohnson61@gmail.com>
2018-10-11 11:18:12 -0700

    Started working on 2b

--- hw1task1.py
+++ hw1task1.py
@@ -6,7 +6,7 @@ URL3 = "http://www.cs.ucsb.edu/~tessaro/cs177/hw/cipher3.txt"
 URLs = {URL1:"", URL2:"", URL3:""}
 
 def shiftDecode(text):
-    text.upper()
+    text = text.upper()
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     print(text)
     for i in range(26):
@@ -17,16 +17,49 @@ def shiftDecode(text):
         print(str(i) + " ")
         print(newText)
 
-for entry in URLs:
-    data = urllib.request.urlopen(entry)
-    cipherText = data.read().decode("utf-8")
-    URLs[entry] = cipherText
+def monoSub(text):
+    text = text.rstrip()
+    text = text.upper()
+    print(text)
+    common = "ETAOINSHRDLU"
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    newMapping = ""
+    frequency = {"A":0, "B":0,"C":0,"D":0,"E":0,
+             "F":0,"G":0,"H":0,"I":0,"J":0,
+             "K":0,"L":0,"M":0,"N":0,"O":0,
+             "P":0,"Q":0,"R":0,"S":0,"T":0,
+             "U":0,"V":0,"W":0,"X":0,"Y":0,
+             "Z":0}
+    for letter in text:
+        frequency[letter] = frequency[letter] + 1
+
+    for commonChar in frequency:
+        min = 0
+        minLetter = ""
+        if(len(newMapping)<12):
+            newMapping += commonChar
+        else:
+            if(min < frequency[commonChar]):
+                #do something
+
+
+    for c in frequency:
+        print(c + ": " + str(frequency[c]))
+    print(newMapping)
+
+def setupURLs(URLs):
+    for entry in URLs:
+        data = urllib.request.urlopen(entry)
+        cipherText = data.read().decode("utf-8")
+        URLs[entry] = cipherText
+
+setupURLs(URLs)
 
 for url in URLs:
     print(URLs[url])
 
-
-
-
-
+#Task 2a
 shiftDecode(URLs[URL1])
+
+#Task 2b
+monoSub(URLs[URL2])
