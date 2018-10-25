fcc7f29e28334ba27bbd7da49a38a0961f2677aa
Blake Johnson <brjohnson61@gmail.com>
2018-10-25 16:50:33 -0700

    Password crack updated

---
+++ passwordCrack.py
@@ -0,0 +1,72 @@
+import crypt
+
+with open('shadow', 'r') as f:
+    data = f.readlines()
+
+data = [line.strip() for line in data]
+
+
+usernames = []
+salts = []
+hashes = [] 
+
+for hash in data:
+    
+    usernames.append(hash.split(':')[0])
+    hashes.append(hash.split(':')[1])
+    if("$" in hash):
+        salt = "$" + hash.split("$")[1] + "$" + hash.split("$")[2] + "$"
+        salts.append(salt)
+    else:
+        salts.append("") 
+
+print(usernames)
+print(salts)
+
+characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-+=/?><.,~`"
+
+for i in range(len(usernames)):
+    user = usernames[i]
+    salt = salts[i]
+    myguess = ""
+    myhash = hashes[i]
+    maxLength = 20
+
+
+    passwordFind(maxLength, user, salts[i], myguess, myhash)
+
+    # for i in range(len(characters)):
+        
+    #     guess0 = guess + characters[i]
+    #     if(checkPassword(user, salt, guess0, hash)):
+    #         break
+    #     for i in range(len(characters)):
+    #         guess1 = guess0 + characters[i]
+    #         if(checkPassword(user, salt, guess1, hash)):
+    #             break
+    #         for i in range(len(characters)):
+    #             guess2 = guess1 + characters[i]
+    #             if(checkPassword(user, salt, guess0, hash)):
+    #                 break
+                
+
+def passwordFind(maxPasswordLength, username, salt, guess, myhash):
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-+=/?><.,~`"
+    for i in range(len(characters)):
+        guess0 = guess + characters[i]
+        if(checkPassword(user, salt, guess0, myhash)):
+            return True
+        else:
+            if(maxPasswordLength > 1):
+                if(passwordFind(maxPasswordLength-1, username, salt, guess0, myhash)):
+                    return True
+                else:
+                    return False
+                    
+def checkPassword(user, password, salt, hash):
+    e_pass = crypt.crypt(password, salt)
+    if(e_pass in hash):
+        print("User: " + user + ", Password: " + password)
+        return True
+    else:
+        return False
